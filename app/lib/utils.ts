4543d80b2ebbf33f398dc08d80a91a05aa778df9
Ondřej Velíšek <ondrejvelisek@gmail.com>
2024-09-15 21:59:35 +0200

    fix: mobile scale and add seo

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,40 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function seo({
+  title,
+  description,
+  keywords,
+  image,
+}: {
+  title: string;
+  description?: string;
+  image?: string;
+  keywords?: string;
+}) {
+  const tags = [
+    { title },
+    { name: "description", content: description },
+    { name: "keywords", content: keywords },
+    { name: "twitter:title", content: title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:creator", content: "@tannerlinsley" },
+    { name: "twitter:site", content: "@tannerlinsley" },
+    { name: "og:type", content: "website" },
+    { name: "og:title", content: title },
+    { name: "og:description", content: description },
+    ...(image
+      ? [
+          { name: "twitter:image", content: image },
+          { name: "twitter:card", content: "summary_large_image" },
+          { name: "og:image", content: image },
+        ]
+      : []),
+  ];
+
+  return tags;
 }
--- app/pages/~__root.tsx
+++ app/pages/~__root.tsx
@@ -10,9 +10,29 @@ import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
 import mainCss from "../main.css?url";
 
 import { StrictMode } from "react";
+import { seo } from "@/lib/utils";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  meta: () => [
+    {
+      charSet: "utf-8",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    ...seo({
+      title:
+        "Rozpočet národa | Státní rozpočet České republiky ve Vašich rukou",
+      description: `Prozkoumejte státní rozpočet do poslední koruny.
+      Rozpočet národa je interaktivní aplikace,
+      která Vám umožní prozkoumat státní rozpočet České republiky a zjistit, kam putují.
+      Vyzkoušejte si, jak byste rozdělili peníze Vy! Sdílejte své nápady a názory s ostatními.`,
+      keywords:
+        "Rozpočet národa, rozpočet, státní rozpočet, finance, peníze, česká republika, interaktivní, výdaje, příjmy",
+    }),
+  ],
   links: () => [{ rel: "stylesheet", href: mainCss }],
 });
 
