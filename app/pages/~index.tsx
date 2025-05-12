5456bfbe03cfe2c40992976f94bf25b76326e434
Ondřej Velíšek <ondrejvelisek@gmail.com>
2025-05-12 14:16:19 +0200

    Update home text

--- app/pages/~index.tsx
+++ app/pages/~index.tsx
@@ -7,28 +7,63 @@ export const Route = createFileRoute("/")({
 function Page() {
   return (
     <>
-      <h1 className="whitespace-nowrap px-8 pt-8 font-serif text-4xl lg:hidden">
-        Tak kam s&nbsp;tím?
-      </h1>
-      <div className="mb-8 flex aspect-square max-h-full items-center bg-hero-img-square bg-cover bg-no-repeat lg:aspect-video lg:bg-hero-img">
-        <div className="hidden w-5/12 pl-12 lg:block">
-          <h1 className="pb-8 font-serif text-6xl">Tak&nbsp;kam s&nbsp;tím?</h1>
-          <p className="hidden text-sm md:block  md:text-base xl:text-lg">
-            Rozpočet národa je interaktivní aplikace, která Vám umožní
-            prozkoumat státní rozpočet České republiky a zjistit, kam putují
-            naše peníze. Vyzkoušejte si, jak byste rozdělili peníze Vy! Sdílejte
-            své nápady a názory s ostatními.
+      <div className="lg:hidden">
+        <h1 className="whitespace-nowrap px-6 pt-6 font-serif text-4xl">
+          Tak kam s&nbsp;tím?
+        </h1>
+        <img
+          src="./public/hero-img-square.jpg"
+          alt="Ilustrace českého lva s kolečky plných peněz"
+          className="pb-6"
+        />
+        <p className="px-6 pb-4">
+          Každý z nás přisypeme každý měsíc na společnou hromádku spoustu peněz.
+          Daň z příjmů, sociální a zdravotní, DPH, spotřební daně a spoustu
+          dalších. Dohromady jsme za rok 2024 nahromadili 2,5 bilionů korun.
+          Kdybychom nic z toho nezaplatili, každý si vyděláme víc než
+          dvojnásobek.
+        </p>
+        <p className="px-6 pb-4">
+          Co se s tou hromadou peněz děje? Nikdo z nás nemusí uklízet chodníky
+          po kterých chodíme. Můžeme jezdit po silnicích. Sedáme na lavičkách v
+          parku. Dostáváme starobní důchod. Za většinu zdravotních služeb
+          nemusíme platit. A když se něco stane, můžeme kdykoliv zavolat 112.
+        </p>
+        <p className="px-6 pb-4">
+          Kolik mě která z těch služeb vlastně stojí? Potřebujeme všechny?
+          Můžeme někde ušetřit? A nebo některou službu přidat?
+        </p>
+      </div>
+      <div className="mb-8 hidden items-center bg-hero-img bg-contain bg-right bg-no-repeat lg:block">
+        <div className="w-5/12 pl-12">
+          <h1 className="py-8 font-serif text-6xl">Tak&nbsp;kam s&nbsp;tím?</h1>
+          <p className="pb-4 text-base">
+            Každý z nás přisypeme každý měsíc na společnou hromádku spoustu
+            peněz. Daň z příjmů, sociální a zdravotní, DPH, spotřební daně a
+            spoustu dalších. Dohromady jsme za rok 2024 nahromadili 2,5 bilionů
+            korun. Kdybychom nic z toho nezaplatili, každý si vyděláme víc než
+            dvojnásobek.
+          </p>
+          <p className="pb-4 text-base">
+            Co se s tou hromadou peněz děje? Nikdo z nás nemusí uklízet chodníky
+            po kterých chodíme. Můžeme jezdit po silnicích. Sedáme na lavičkách
+            v parku. Dostáváme starobní důchod. Za většinu zdravotních služeb
+            nemusíme platit. A když se něco stane, můžeme kdykoliv zavolat 112.
+          </p>
+          <p className="text-base">
+            Kolik mě která z těch služeb vlastně stojí? Potřebujeme všechny?
+            Můžeme někde ušetřit? A nebo některou službu přidat?
           </p>
         </div>
       </div>
 
-      <div className="p-8 lg:p-12">
-        <h1 className="pb-8 font-serif text-4xl">O projektu</h1>
+      <div className="p-6 lg:p-12">
+        <h1 className="pb-6 font-serif text-4xl">O projektu</h1>
         <p>
           Rozpočet národa je interaktivní aplikace, která Vám umožní prozkoumat
           státní rozpočet České republiky a zjistit, kam putují naše peníze.
-          Vyzkoušejte si, jak byste rozdělili peníze Vy! Sdílejte své nápady a
-          názory s ostatními.
+          Kolik za to všechno vy osobně zaplatíte. Vyzkoušejte si, jak byste
+          rozdělili peníze Vy a sdílejte své nápady a názory s ostatními.
         </p>
       </div>
     </>
